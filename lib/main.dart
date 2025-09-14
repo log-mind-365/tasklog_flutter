d5674cc8bea4a1470353bd8daafad66e4c5809f9
kidboi666 <kidboi666@gmail.com>
2025-09-14 14:38:06 +0900

    feat: Riverpod 및 Dio 패키지 추가 설정
    - `flutter_riverpod`와 `dio` 패키지를 `pubspec.yaml`에 추가하여 상태 관리 및 네트워크 통신 기능을 구현합니다.
    - `main.dart`에 `ProviderScope`를 추가하고 `ConsumerWidget`을 사용하여 Riverpod를 통합합니다.
    - `analysis_options.yaml`에 `custom_lint` 플러그인을 추가하여 코드 분석 기능을 강화합니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tasklog_flutter/core/ui/theme.dart';
 
+final helloWorldProvider = Provider((_) => 'Hello World');
+
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,51 +17,30 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme(),
       darkTheme: AppTheme.darkTheme(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(helloWorldProvider);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(value)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
